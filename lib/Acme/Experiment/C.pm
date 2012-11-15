615148f3bf49165fe176799a98a5521c0c61a9bc
Olaf Alders <olaf@wundersolutions.com>
2012-11-15 08:37:27 -0500

    initial commit

---
+++ lib/Acme/Experiment/C.pm
@@ -0,0 +1,3 @@
+package ACME::Experiment::C;
+
+1;
---
+++ lib/Acme/Experiment/OALDERS.pm
@@ -0,0 +1,7 @@
+use strict;
+use warnings;
+package Acme::Experiment::OALDERS;
+
+1;
+
+# ABSTRACT: Experimental module to test PAUSE permissions.
